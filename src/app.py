53b29b827c09f85832584fb4bc19cc9b22e4fca4
David Wampamba <davidwampamba@gmail.com>
2021-05-25 18:33:05 +0300

    Source code added to app.py

--- src/app.py
+++ src/app.py
@@ -1,132 +1,19 @@
-from types import MethodDescriptorType
-from flask import Flask, jsonify,request
-from flask_cors import CORS, cross_origin
+"""Flask Application"""
 
-from bson.objectid import ObjectId
-from db import connect
-import members
+# load libraries
+from flask import Flask, jsonify
+import sys
 
-connection = connect()
-db = connection.shineafrika
+from flask.helpers import url_for
+
+# load modules
+from src.endpoints.products import products
+from src.endpoints.members import members
+
+# init Flask app
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/")
-def index():
-    return "Ooops! Nothing here"
-
-@app.route('/products', methods=['GET'])
-def getProducts():
-    json_products = []
-    products = db.products.find()
-
-    for product in products:
-        json_products.append({
-            'id': str(product['_id']),
-            'title':product['title'],
-            'expires': product['expires'],
-            'customer': product['customer'],
-            'price': product['price'],
-        })
-
-    response = jsonify({
-        'result':'successful',
-        'products': json_products
-    })
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
-
-@app.route("/products", methods=['POST'])
-@cross_origin()
-def createProduct():
-    product_data = request.get_json()
-    product = db.products.insert_one({
-        'title': product_data['title'],
-        'customer' : product_data['customer'],
-        'price' : product_data['price'],
-        'expires' : product_data['expires']
-    })
-
-    productID = product.inserted_id
-    productFound = db.products.find_one({'_id': ObjectId(productID)})
-    productJSON = {
-        'id': str(productFound['_id']),
-        'customer': productFound['customer'],
-        'title': productFound['title'],
-        'price': productFound['price'],
-        'expires': productFound['expires']
-    }
-
-    return jsonify({
-        'result':'successful',
-        'product': productJSON
-    })
-
-@app.route("/products/<id>", methods=['GET'])
-def getProduct(id):
-    product = db.products.find_one({'_id': ObjectId(id)})
-    json_product = {
-        'id': str(product['_id']),
-        'customer': product['customer'],
-        'title': product['title'],
-        'price': product['price'],
-        'expires': product['expires']
-    }
-
-    return jsonify({
-        'result':'successful',
-        'product':json_product
-    })
-
-@app.route('/products', methods=['PUT'])
-def updateProduct():
-    products = request.get_json()
-    result = db.products.update_one({'_id': ObjectId(products['id'])}, {"$set": products['info']})
-
-    if(result.modified_count > 0):
-        return jsonify({
-            'result': 'successful',
-            'msg': 'Updated'
-        })
-    
-    return jsonify({
-        'result': 'failure'
-    })
-
-
-@app.route('/products', methods=['DELETE'])
-def deleteProduct():
-    products = request.get_json()
-    result = db.products.delete_one({'_id' : ObjectId(products['id'])})
-    
-    if result.deleted_count == 1:
-        response = f"{result.deleted_count} Product deleted"
-    elif result.deleted_count > 1:
-        response = f"{result.deleted_count} Products deleted"
-    else:
-        response = "Product to delete Not Found"
-        return jsonify({
-        'result': 'failure',
-        'msg' : response
-        })
-    
-    return jsonify({
-        'result': 'successful',
-        'msg':response
-    })
-
-@app.route('/members', methods=['GET'])
-def returnMembers():
-    return members.retrieveMembers()
-
-@app.route('/login', methods=['POST'])
-def Login():
-    return members.login()
-
-@app.route('/members', methods=['POST'])
-def 
-
-if __name__ == '__main__':
-    app.run()
+# register blueprints. ensure that all paths are versioned!
+app.register_blueprint(products, url_prefix="/api/v1/products")
+app.register_blueprint(members, url_prefix="/api/v1/members")
---
+++ src/blueprints/products.py
@@ -0,0 +1,129 @@
+from types import MethodDescriptorType
+from flask import Flask, jsonify,request
+from flask_cors import CORS, cross_origin
+
+from bson.objectid import ObjectId
+from db import connect
+import members
+
+connection = connect()
+db = connection.shineafrika
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+def index():
+    return "Ooops! Nothing here"
+
+@app.route('/products', methods=['GET'])
+def getProducts():
+    json_products = []
+    products = db.products.find()
+
+    for product in products:
+        json_products.append({
+            'id': str(product['_id']),
+            'title':product['title'],
+            'expires': product['expires'],
+            'customer': product['customer'],
+            'price': product['price'],
+        })
+
+    response = jsonify({
+        'result':'successful',
+        'products': json_products
+    })
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route("/products", methods=['POST'])
+@cross_origin()
+def createProduct():
+    product_data = request.get_json()
+    product = db.products.insert_one({
+        'title': product_data['title'],
+        'customer' : product_data['customer'],
+        'price' : product_data['price'],
+        'expires' : product_data['expires']
+    })
+
+    productID = product.inserted_id
+    productFound = db.products.find_one({'_id': ObjectId(productID)})
+    productJSON = {
+        'id': str(productFound['_id']),
+        'customer': productFound['customer'],
+        'title': productFound['title'],
+        'price': productFound['price'],
+        'expires': productFound['expires']
+    }
+
+    return jsonify({
+        'result':'successful',
+        'product': productJSON
+    })
+
+@app.route("/products/<id>", methods=['GET'])
+def getProduct(id):
+    product = db.products.find_one({'_id': ObjectId(id)})
+    json_product = {
+        'id': str(product['_id']),
+        'customer': product['customer'],
+        'title': product['title'],
+        'price': product['price'],
+        'expires': product['expires']
+    }
+
+    return jsonify({
+        'result':'successful',
+        'product':json_product
+    })
+
+@app.route('/products', methods=['PUT'])
+def updateProduct():
+    products = request.get_json()
+    result = db.products.update_one({'_id': ObjectId(products['id'])}, {"$set": products['info']})
+
+    if(result.modified_count > 0):
+        return jsonify({
+            'result': 'successful',
+            'msg': 'Updated'
+        })
+    
+    return jsonify({
+        'result': 'failure'
+    })
+
+
+@app.route('/products', methods=['DELETE'])
+def deleteProduct():
+    products = request.get_json()
+    result = db.products.delete_one({'_id' : ObjectId(products['id'])})
+    
+    if result.deleted_count == 1:
+        response = f"{result.deleted_count} Product deleted"
+    elif result.deleted_count > 1:
+        response = f"{result.deleted_count} Products deleted"
+    else:
+        response = "Product to delete Not Found"
+        return jsonify({
+        'result': 'failure',
+        'msg' : response
+        })
+    
+    return jsonify({
+        'result': 'successful',
+        'msg':response
+    })
+
+@app.route('/members', methods=['GET'])
+def returnMembers():
+    return members.retrieveMembers()
+
+@app.route('/login', methods=['POST'])
+def Login():
+    return members.login()
+
+if __name__ == '__main__':
+    app.run()
